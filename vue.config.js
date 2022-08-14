61b7d2258e74a989a8321e9f5f1d8f90a655d0c9
codesyariah122 <pujiermanto@gmail.com>
2022-08-14 15:31:56 +0700

    update env config

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,9 @@ module.exports = {
 		icons: {
 			favicon32: 'img/icons/favicon-32x32.png',
 			favicon16: 'img/icons/favicon-16x16.png',
-			appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-			maskIcon: 'img/icons/safari-pinned-tab.svg',
-			msTileImage: 'img/icons/msapplication-icon-144x144.png'
+			appleTouchIcon: 'img/icons/apple-icon-152x152.png',
+			maskIcon: 'img/icons/maskable_icon_x192.png',
+			msTileImage: 'img/icons/ms-icon-144x144.png'
 		}
 	},
 	css: {
